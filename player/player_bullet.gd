df652f07599eaf8059c9acf0adb07108a3e03b9f
italiatroller-1990 <italiatroller@gmail.com>
2024-09-22 19:39:48 +0700

    src added

---
+++ player/player_bullet.gd
@@ -0,0 +1,25 @@
+extends Area3D
+
+# Speed of the projectile
+var speed: float = 50.0
+var lifetime: float = 3.0  # Time before the projectile is removed
+
+# Called when the projectile is instantiated
+func _ready() -> void:
+	# Start the movement
+	set_as_top_level(true)  # Ensure it moves independently of parent transformations
+	
+	# Start a timer to remove the projectile
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
+
+func _process(delta: float) -> void:
+	# Move the projectile forward
+	position += -transform.basis.z * speed * delta
+
+func _on_area_entered(area: Area3D) -> void:
+	print("solid hit!")
+	queue_free()
+	if area.is_in_group("enemy"):
+		print("enemy hit!")
+		queue_free()
